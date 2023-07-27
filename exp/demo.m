264841c749d6076c4cf8f7725587a10ee75c48d3
JPickard1 <jpic@umich.edu>
2023-07-27 03:03:04 -0400

    demo file

---
+++ exp/demo.m
@@ -0,0 +1,13 @@
+%% Demo
+%
+% Auth: Joshua Pickard
+%       jpic@umich.edu
+% Date: July 27, 2023
+
+A = rand(2,4)
+
+d = 2
+
+% LyapProduct(A, kronexp(rand(4,1), 2), d)
+e = 2;
+LyapProduct(rand(n,n^e), kronexp(rand(n,1), e), d)
